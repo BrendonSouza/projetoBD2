be9784a7ae8c487400ab453d2e6c445483f544b4
Breno Rios <breno@brenorios.com.br>
2022-07-06 19:22:14 -0300

    Primeira versão do projeto

---
+++ db/db.js
@@ -0,0 +1,12 @@
+var mysql = require("mysql");
+
+var conexao = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "bd_metrica_ate",
+    multipleStatements: true
+});
+
+conexao.connect();
+module.exports = conexao;
