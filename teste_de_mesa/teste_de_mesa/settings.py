1b571f9db633ac1215418e31034d43cc1bf7535a
Fabiano Santos <58434562+FabioFSS@users.noreply.github.com>
2022-07-07 20:47:46 -0300

    fix language

--- teste_de_mesa/teste_de_mesa/settings.py
+++ teste_de_mesa/teste_de_mesa/settings.py
@@ -116,7 +116,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'pt-be'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'America/Sao_Paulo'
 
