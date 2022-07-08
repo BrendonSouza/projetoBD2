e05474cd847368260492a7c045d7e516e0ec39f5
Fabiano Santos <58434562+FabioFSS@users.noreply.github.com>
2022-07-08 13:20:42 -0300

    fix textarea

--- teste_de_mesa/core/static/js/scripts.js
+++ teste_de_mesa/core/static/js/scripts.js
@@ -1,14 +1,15 @@
-const textarea = document.querySelector("textarea");
-textarea.addEventListener("keydown", function (event) {
-    if (event.key === "Tab") {
-        event.preventDefault();
-        const start = textarea.selectionStart;
-        const end = textarea.selectionEnd;
-        textarea.value =
-            textarea.value.substring(0, start) +
-            "\t" +
-            textarea.value.substring(end);
-        textarea.selectionStart = start + 1;
-        textarea.selectionEnd = start + 1;
-    }
+document.querySelectorAll("textarea").forEach(textarea => {
+    textarea.addEventListener("keydown", function (event) {
+        if (event.key === "Tab") {
+            event.preventDefault();
+            const start = textarea.selectionStart;
+            const end = textarea.selectionEnd;
+            textarea.value =
+                textarea.value.substring(0, start) +
+                "\t" +
+                textarea.value.substring(end);
+            textarea.selectionStart = start + 1;
+            textarea.selectionEnd = start + 1;
+        }
+    });
 });
